233314c8d1ae51a452a7d250efd69889fa38db9b
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-15 11:14:14 +0700

    useSelector, useDispatch Redux Hook

--- src/App.js
+++ src/App.js
@@ -8,34 +8,29 @@ import {
 } from "./action/actions"
 import store from './redux/store';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 function App(props) {
 
-  // người dùng fire một event thì phải nhấn vào một cái nút 
-  // gọi đến hàm đó và xử lý sự kiện nhấn nút 
+  const newCount = useSelector((state) => state.counter123.count)
+  const name = useSelector((state) => state.counter123.name);
+  const dispatch = useDispatch();
 
-
-  // event handler
   const handleIncrease = () => {
-    // dispatch action  --> bắn từ react vào trong store 
-    props.increaseCounter()
-
-    //   store.dispatch({
-    //     type: 'Test', 
-    //     payload: {'name' : 'lam'}              // ---> cách viết này giống như cách trên, cũng dispatch một action
-    //   }                                        // nhưng cách viết trên ngắn gọn hơn được redux hỗ trợ để chúng ta code ít đi 
-    // )                                          // nhờ có thằng connect của thư viện react/redux 
-                                                  // và mapDispatchToProps 
+    
+    dispatch(increaseCounter())
+    
   }
 
   return (
     <div className="App">
       <header className="App-header">
-      <div>Count: {props.count}</div>
-      <div>hello: {props.name}</div>
+      <div>Count: {newCount}</div>
+      <div>hello: {name}</div>
 
       <button onClick={() => handleIncrease()}>Increase Count</button>
 
-      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+      <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
       </header>
     </div>
   );
@@ -43,23 +38,25 @@ function App(props) {
 
 
 
-// map state của thằng ( Redux ) cho vào props của thằng ( React )
-const mapStateToProps = state => {
-  return {
-    count: state.counter123.count,
-    name: state.counter123.name
-  }
-}
+// // map state của thằng ( Redux ) cho vào props của thằng ( React )
+// const mapStateToProps = state => {
+//   return {
+//     count: state.counter123.count,
+//     name: state.counter123.name
+//   }
+// }
 
-// map dispatch (redux) to props react 
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
+// // map dispatch (redux) to props react 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  }
-}
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   }
+// }
 
-// connect là một HOC 
-// cấy hàm hai function vào Component App 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+// // connect là một HOC 
+// // cấy hàm hai function vào Component App 
+// export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+export default App
