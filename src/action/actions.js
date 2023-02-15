642daec3d0ebe6fc394b9e3b150de62b0986d660
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-15 08:49:32 +0700

    setup redux origin

---
+++ src/action/actions.js
@@ -0,0 +1,13 @@
+import { INCREMENT, DECREMENT } from "./constant";
+
+export const increaseCounter = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decreaseCounter = () => {
+  return {
+    type: DECREMENT,
+  };
+};
