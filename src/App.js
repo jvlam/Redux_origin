94f5fdfdaf505b3c784beb2ef09849fa94b17abe
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-15 09:47:08 +0700

    dispatch

--- src/App.js
+++ src/App.js
@@ -6,20 +6,41 @@ import {
   increaseCounter,
   decreaseCounter,
 } from "./action/actions"
+import store from './redux/store';
 
 function App(props) {
+
+  // người dùng fire một event thì phải nhấn vào một cái nút 
+  // gọi đến hàm đó và xử lý sự kiện nhấn nút 
+
+
+  // event handler
+  const handleIncrease = () => {
+    // dispatch action  --> bắn từ react vào trong store 
+    // props.increaseCounter()
+
+      store.dispatch({
+        type: 'Test', 
+        payload: {'name' : 'lam'}              // ---> cách viết này giống như cách trên, cũng dispatch một action
+      }                                        // nhưng cách viết trên ngắn gọn hơn được redux hỗ trợ để chúng ta code ít đi 
+    )                                          // nhờ có thằng connect của thư viện react/redux 
+                                               // và mapDispatchToProps 
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       <div>Count: {props.count}</div>
 
-      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+      <button onClick={() => handleIncrease()}>Increase Count</button>
 
       <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
       </header>
     </div>
   );
 }
+
+
 
 
 const mapStateToProps = state => {
